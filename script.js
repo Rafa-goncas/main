ebbd41879dffa39ab3269204763828772754f3c4
Rafa-goncas <rafaelrgoncalves04@gmail.com>
2024-11-17 10:06:29 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("contact-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Obrigado por entrar em contato! Em breve, responderei sua mensagem.");
+});
